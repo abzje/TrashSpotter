53a26a4de89ad9d30cb409b80f5d3b67a90d930d
G3-P19\utilisateur <loicjacob.pro@gmail.com>
2021-05-05 14:52:00 +0200

    add prefix changing

--- TrashSpotter/Assets/TrashSpotter/Scripts/UI/Screens/PopUp/PrefixNamePopUp.cs
+++ TrashSpotter/Assets/TrashSpotter/Scripts/UI/Screens/PopUp/PrefixNamePopUp.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Com.TrashSpotter
 {
     public class PrefixNamePopUp : PopUp
     {
-       
+        [SerializeField] private GameObject nameContainer = null;
+
+        [SerializeField] private GameObject prefixContainer = null;
+
+        private void Start()
+        {
+            foreach (Toggle item in prefixContainer.transform.GetComponentsInChildren<Toggle>())
+            {
+                item.onValueChanged.AddListener((value) => ChangePrefix(item.GetComponent<Text>()));
+            }
+        }
+
+        public override void Open()
+        {
+            base.Open();
+
+            Debug.Log("Set layer order of " + nameContainer.name + " at front");
+
+            
+        }
+
+        private void ChangePrefix(Text text)
+        {
+            for (int i = 0; i < nameContainer.transform.childCount; i++)
+            {
+                if (nameContainer.transform.GetChild(i).name == "NamePrefix")
+                {
+                    nameContainer.transform.GetChild(i).GetComponentInChildren<Text>().text = text.text;
+                }
+            }
+        }
     }
 }
