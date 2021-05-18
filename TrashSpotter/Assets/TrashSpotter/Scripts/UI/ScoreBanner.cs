4cf212023b06d7ac6a099dfcf995710f0f04df79
G3-P19\utilisateur <loicjacob.pro@gmail.com>
2021-05-18 12:44:53 +0200

    fix score banner

---
+++ TrashSpotter/Assets/TrashSpotter/Scripts/UI/ScoreBanner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBanner : MonoBehaviour
+{
+    [SerializeField] private Image seedFiller = null;
+    [SerializeField] private Image levelFiller = null;
+    [SerializeField] private Text seedText = null;
+    [SerializeField] private Text levelText = null;
+    [SerializeField] private Text moneyText = null;
+    [SerializeField] private GameObject seedContainer = null;
+}
