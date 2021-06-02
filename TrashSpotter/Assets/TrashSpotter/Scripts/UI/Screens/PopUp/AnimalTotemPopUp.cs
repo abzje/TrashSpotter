c431661f9d84035eeab7d61e8d9a8f1050851fa7
DESKTOP-33UOQG4\LOIC <l.jacob@student.isartdigital.com>
2021-06-02 11:21:44 +0200

    add background button

--- TrashSpotter/Assets/TrashSpotter/Scripts/UI/Screens/PopUp/AnimalTotemPopUp.cs
+++ TrashSpotter/Assets/TrashSpotter/Scripts/UI/Screens/PopUp/AnimalTotemPopUp.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace Com.TrashSpotter
 {
-    public class AnimalTotemPopUp : Screen
+    public class AnimalTotemPopUp : PopUp
     {
         [Header("Animal Details Settings")]
         [SerializeField] private Image favoriteImage = null;
         [SerializeField] private Image animalImage = null;
         [SerializeField] private Text animalNameText = null;
         [SerializeField] private Text animalDescriptionText = null;
-	}
+
+        [Header("General Settings")]
+        [SerializeField] private Button backgroundButton = null;
+
+        private void Start()
+        {
+            backgroundButton.onClick.AddListener(OnClickQuit);
+        }
+
+        private void OnClickQuit()
+        {
+            UIManager.Instance.ClosePopUp(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            backgroundButton.onClick.RemoveListener(OnClickQuit);
+        }
+    }
 }
